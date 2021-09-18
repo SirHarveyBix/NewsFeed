86ce879ef9020d03915f5b7eedf9625a460fa26e
Guillaume <Gui.lefort.17@gmail.com>
2021-09-18 12:47:56 +0200

    init query feed, feeds

--- utils/api/resolvers.ts
+++ utils/api/resolvers.ts
@@ -3,5 +3,7 @@ export const resolvers = {
     hello: (_parent: any, _args: any, _context: any) => 'hi!',
     id: (_parent: any, _args: any, _context: any) =>
       "it is supposed to work fine with 'hello' query",
+    feed: (parent, { data: { id } }, { prisma }) => prisma.feed.findUnique({ where: { id } }),
+    feeds: (parent, args, { prisma }) => prisma.feed.findMany(),
   },
 };
