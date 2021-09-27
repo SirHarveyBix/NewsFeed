3251ad9e16430b6a161df7252c7c3c0b5ada1f93
Guillaume <Gui.lefort.17@gmail.com>
2021-09-27 09:42:14 +0200

    add Item list for bundle and feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { ItemList } from '../components/ItemList';
 import { Layout } from '../components/Layout';
+import { ItemType } from '../utils/types';
 
 const Index = () => {
   return (
     <Layout>
       <h3 className="justify-start flex text-lg font-medium py-4">Home Page</h3>
+      <ItemList type={ItemType.BundleType} />
     </Layout>
   );
 };
